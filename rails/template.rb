699c2963728a34457b377247379c3631155d6a2d
Matthew Todd <matthew.todd@gmail.com>
2010-01-12 13:40:20 +0100

    Configure test libraries.

--- rails/template.rb
+++ rails/template.rb
@@ -4,6 +4,12 @@ class ::String
   end
 end
 
+def append_sentinel(file, sentinel, data)
+  gsub_file(file, /(#{Regexp.escape(sentinel)})/mi) do |match|
+    "#{match}\n#{data}"
+  end
+end
+
 def gitignore(pattern)
   append_file('.gitignore', "#{pattern}\n")
   run("sort -u -o .gitignore .gitignore")
@@ -14,6 +20,7 @@ end
   basic.rb
   bundler.rb
   cucumber.rb
+  testing.rb
   variables.rb
 ).map do |file|
   File.join(ENV['HOME'], '.rails', 'templates', file)
---
+++ rails/templates/testing.rb
@@ -0,0 +1,25 @@
+append_file('Gemfile', <<-END.unindent)
+
+  only(:test) do
+    gem 'faker',     '0.3.1'
+    gem 'machinist', '1.0.6', :require_as => 'machinist/active_record'
+    gem 'shoulda',   '2.10.2'
+  end
+END
+
+run('gem bundle')
+
+file('test/blueprints.rb')
+
+append_sentinel('test/test_helper.rb', "require 'test_help'", <<-END.unindent)
+  require File.expand_path(File.dirname(__FILE__) + '/blueprints')
+END
+
+gsub_file('test/test_helper.rb', /^.*fixtures.*$\n/, '')
+
+git(:add => 'Gemfile')
+git(:add => 'test/blueprints.rb')
+git(:add => 'test/test_helper.rb')
+git(:add => 'vendor/bundler')
+
+git(:commit => '-m "Set up test libraries."')
