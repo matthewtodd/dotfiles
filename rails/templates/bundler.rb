d45c48019e783fbaaeb0a623b8227acc92645d1b
Matthew Todd <matthew.todd@gmail.com>
2010-01-29 12:30:53 +0300

    Heroku supports bundler!

--- rails/templates/bundler.rb
+++ rails/templates/bundler.rb
@@ -1,5 +1,24 @@
 append_file('config/preinitializer.rb', <<-END.unindent)
   require 'vendor/bundle/environment.rb'
+
+  # http://docs.heroku.com/gems#gem-bundler
+  class Rails::Boot
+    def run
+      load_initializer
+      extend_environment
+      Rails::Initializer.run(:set_load_path)
+    end
+
+    def extend_environment
+      Rails::Initializer.class_eval do
+        old_load = instance_method(:load_environment)
+        define_method(:load_environment) do
+        Bundler.require_env RAILS_ENV
+          old_load.bind(self).call
+        end
+      end
+    end
+  end
 END
 
 git(:add => 'config/preinitializer.rb')
