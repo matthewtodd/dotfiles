d8e24882f866d692f067228f17c848f5eff88e46
Matthew Todd <matthew@matthewtodd.org>
2020-01-02 08:13:46 -0500

    Compose functions for window movement.

--- etc/hammerspoon/init.lua
+++ etc/hammerspoon/init.lua
@@ -30,38 +30,47 @@ caffeine:setClickCallback(function()
   caffeine:setTitle(caffeineTitle(hs.caffeinate.toggle("displayIdle")))
 end)
 
-hs.window.animationDuration = 0
+function thread(...)
+  local stages = table.pack(...)
 
-function move(x, w, h)
-  return function()
-    local window = hs.window.focusedWindow()
-    local screen = window:screen():frame()
-    local frame = window:frame()
-    frame.x = x(screen, w) + 6
-    frame.y = screen.y + 6
-    frame.w = (screen.w * w) - 12
-    frame.h = (screen.h * h) - 12
-    window:setFrame(frame)
+  return function(seed)
+    local result = seed
+
+    for _,stage in ipairs(stages) do
+      result = stage[1](result, table.unpack(stage, 2))
+    end
+
+    return result
   end
 end
 
-function left(screen, w)
-  return screen.x
+hs.window.animationDuration = 0
+
+function move(position)
+  return function()
+    local window = hs.window.focusedWindow()
+    local screen = window:screen()
+    window:setFrame(position(screen:frame()))
+  end
 end
 
-function center(screen, w)
-  return screen.x + screen.w * (1 - w) / 2
+function position(frame, unit)
+  return hs.geometry.new(unit):fromUnitRect(frame)
 end
 
-function right(screen, w)
-  return screen.x + screen.w * (1 - w)
+function margin(frame, size)
+  return hs.geometry.new{
+    x = frame.x + size,
+    y = frame.y + size,
+    w = frame.w - size * 2,
+    h = frame.h - size * 2
+  }
 end
 
 -- TODO modal hotkeys?
-hs.hotkey.bind('⌃⌥⌘', 'L', move(left, 1/3, 5/6))
-hs.hotkey.bind('⌃⌥⌘', 'C', move(center, 1/3, 11/12))
-hs.hotkey.bind('⌃⌥⌘', 'M', move(center, 1/2, 11/12))
-hs.hotkey.bind('⌃⌥⌘', 'R', move(right, 1/3, 5/6))
-hs.hotkey.bind('⌃⌥⇧⌘', 'L', move(left, 1/4, 3/4))
-hs.hotkey.bind('⌃⌥⇧⌘', 'M', move(center, 5/6, 1))
-hs.hotkey.bind('⌃⌥⇧⌘', 'R', move(right, 1/4, 3/4))
+hs.hotkey.bind('⌃⌥⌘', 'L', move(thread({position, {0/3, 0, 1/3, 5/6}}, {margin, 6})))
+hs.hotkey.bind('⌃⌥⌘', 'C', move(thread({position, {1/3, 0, 1/3, 11/12}}, {margin, 6})))
+hs.hotkey.bind('⌃⌥⌘', 'M', move(thread({position, {1/4, 0, 1/2, 11/12}}, {margin, 6})))
+hs.hotkey.bind('⌃⌥⌘', 'R', move(thread({position, {2/3, 0, 1/3, 5/6}}, {margin, 6})))
+hs.hotkey.bind('⌃⌥⇧⌘', 'L', move(thread({position, {0/4, 0, 1/4, 3/4}}, {margin, 6})))
+hs.hotkey.bind('⌃⌥⇧⌘', 'R', move(thread({position, {3/4, 0, 1/4, 3/4}}, {margin, 6})))
