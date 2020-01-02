a08174749914e85e0f21a24a71494db80d9db7d4
Matthew Todd <matthew@matthewtodd.org>
2020-01-01 21:27:02 -0500

    Replace divvy.

--- etc/hammerspoon/init.lua
+++ etc/hammerspoon/init.lua
@@ -29,3 +29,39 @@ caffeine:setTitle(caffeineTitle(hs.caffeinate.get("displayIdle")))
 caffeine:setClickCallback(function()
   caffeine:setTitle(caffeineTitle(hs.caffeinate.toggle("displayIdle")))
 end)
+
+hs.window.animationDuration = 0
+
+function move(x, w, h)
+  return function()
+    local window = hs.window.focusedWindow()
+    local screen = window:screen():frame()
+    local frame = window:frame()
+    frame.x = x(screen, w) + 6
+    frame.y = screen.y + 6
+    frame.w = (screen.w * w) - 12
+    frame.h = (screen.h * h) - 12
+    window:setFrame(frame)
+  end
+end
+
+function left(screen, w)
+  return screen.x
+end
+
+function center(screen, w)
+  return screen.x + screen.w * (1 - w) / 2
+end
+
+function right(screen, w)
+  return screen.x + screen.w * (1 - w)
+end
+
+-- TODO modal hotkeys?
+hs.hotkey.bind('⌃⌥⌘', 'L', move(left, 1/3, 5/6))
+hs.hotkey.bind('⌃⌥⌘', 'C', move(center, 1/3, 11/12))
+hs.hotkey.bind('⌃⌥⌘', 'M', move(center, 1/2, 11/12))
+hs.hotkey.bind('⌃⌥⌘', 'R', move(right, 1/3, 5/6))
+hs.hotkey.bind('⌃⌥⇧⌘', 'L', move(left, 1/4, 3/4))
+hs.hotkey.bind('⌃⌥⇧⌘', 'M', move(center, 5/6, 1))
+hs.hotkey.bind('⌃⌥⇧⌘', 'R', move(right, 1/4, 3/4))
