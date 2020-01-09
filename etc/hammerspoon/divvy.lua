de123638bdb565644354d58b3873f7166609f260
Matthew Todd <matthew@matthewtodd.org>
2020-01-08 20:56:10 -0500

    Tweak laptop layout a little more.

--- etc/hammerspoon/divvy.lua
+++ etc/hammerspoon/divvy.lua
@@ -1,11 +1,9 @@
 local function Workflow()
   local _options_macbook_air_display = {
     {1/8, 0, 3/4, 1},
-    {1/4, 0, 1/2, 1},
-    {1/2, 0, 1/2, 1},
-    {0, 0, 1/2, 1},
-    {0, 0, 7/8, 1},
+    {1/2, 0, 1/2, 7/8},
     {0, 0, 1, 1},
+    {0, 0, 3/4, 1},
   }
 
   local _options_thunderbolt_display = {
