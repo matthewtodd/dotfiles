42c9c25a4bba226f96bea9f36d357ac5680745ae
Matthew Todd <matthew@matthewtodd.org>
2020-03-02 14:58:27 -0500

    Replace chruby-fish.
    It seemed to be broken. And how hard could it be? This was fun!

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,11 +1,20 @@
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
+function chruby_auto -e fish_prompt
+  set ruby_version_file "$PWD/.ruby-version"
+
+  if test \( "$RUBY_VERSION_FILE" != $ruby_version_file \) -a \( -f $ruby_version_file \)
+    set -gx RUBY_VERSION_FILE $ruby_version_file
+    set desired (cat $ruby_version_file)
+    string match -qe $desired $RUBY_ROOT
+    if test $status -ne 0
+      chruby $desired
+    end
+  end
+end
 
 set -x CLICOLOR yes
 set -x EDITOR vim
 set -x FZF_DEFAULT_COMMAND 'fd --hidden --type f'
 set -x LESSHISTFILE -
-set -x RUBIES /usr/local/Cellar/ruby/*
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/Library/Caches
 set -x XDG_DATA_HOME $HOME/Library/Application\ Support
