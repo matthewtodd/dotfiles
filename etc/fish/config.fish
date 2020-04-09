30f36e33c2fb3fd7b9299f1a2dbde9a51a6efbed
Matthew Todd <matthew@matthewtodd.org>
2020-04-09 10:15:29 -0400

    Remove custom chruby scripts.
    They're awesome! But we're going to use Docker instead.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,16 +1,3 @@
-function chruby_auto -e fish_prompt
-  set ruby_version_file "$PWD/.ruby-version"
-
-  if test \( "$RUBY_VERSION_FILE" != $ruby_version_file \) -a \( -f $ruby_version_file \)
-    set -gx RUBY_VERSION_FILE $ruby_version_file
-    set desired (cat $ruby_version_file)
-    string match -qe $desired $RUBY_ROOT
-    if test $status -ne 0
-      chruby $desired
-    end
-  end
-end
-
 set -x CLICOLOR yes
 set -x EDITOR vim
 set -x FZF_DEFAULT_COMMAND 'fd --hidden --type f'
