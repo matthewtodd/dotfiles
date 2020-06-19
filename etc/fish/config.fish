78842a8c7b6e26b81f5d5c327205678a7bf9b3fc
Matthew Todd <matthew@matthewtodd.org>
2020-06-19 10:18:01 -0400

    Use fish_user_paths and make homebrew ruby available.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -2,8 +2,9 @@ set -x CLICOLOR yes
 set -x EDITOR vim
 set -x FZF_DEFAULT_COMMAND 'fd --hidden --type f'
 set -x LESSHISTFILE -
-set -x PATH $PATH $HOME/.local/bin
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/Library/Caches
 set -x XDG_DATA_HOME $HOME/Library/Application\ Support
 set -x VIMINIT ":source $XDG_CONFIG_HOME/vim/vimrc"
+
+set -g fish_user_paths "$HOME/.local/bin" "/usr/local/opt/ruby/bin"
