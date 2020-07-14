bf951d2258265700bd126cf09c64c7e39fae0c7e
Matthew Todd <matthew@matthewtodd.org>
2020-07-14 09:44:28 -0400

    Future project placeholder.

---
+++ libexec/dynamic_wallpaper.swift
@@ -0,0 +1,7 @@
+#!/usr/bin/swift
+
+// https://nshipster.com/macos-dynamic-desktop/
+// seems like the nuts and bolts aren't bad.
+// I'm curious to play with some fade through some of the solarized colors.
+// Not sure what that would look like.
+// Perhaps base01 in the day, base03 at night.
