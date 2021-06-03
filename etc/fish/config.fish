10056d6cb880dd5073ed649e96ef6150893bf1e4
Matthew Todd <todd@cockroachlabs.com>
2021-06-03 12:53:46 -0400

    Update setup for building cockroachdb.
    https://wiki.crdb.io/wiki/spaces/CRDB/pages/73204103/Building+from+source+on+macOS

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -9,4 +9,4 @@ set -x XDG_CACHE_HOME $HOME/Library/Caches
 set -x XDG_DATA_HOME $HOME/Library/Application\ Support
 set -x VIMINIT ":source $XDG_CONFIG_HOME/vim/vimrc"
 
-set -g fish_user_paths "$HOME/.local/bin" "/usr/local/opt/ruby/bin"
+set -g fish_user_paths "$HOME/.local/bin" /usr/local/opt/ruby/bin /usr/local/opt/make/libexec/gnubin
