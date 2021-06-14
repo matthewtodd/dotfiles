9e2f96dd883cca204cc48e6389943ab546b741ac
Matthew Todd <todd@cockroachlabs.com>
2021-06-14 11:00:20 -0400

    Set this to avoid warnings in GoLand.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -4,6 +4,7 @@ set -x FZF_DEFAULT_COMMAND 'fd --hidden --type f'
 set -x GOPATH $HOME/go
 set -x GOROOT /usr/local/opt/go/libexec
 set -x LESSHISTFILE -
+set -x MACOSX_DEPLOYMENT_TARGET (sw_vers -productVersion)
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/Library/Caches
 set -x XDG_DATA_HOME $HOME/Library/Application\ Support
