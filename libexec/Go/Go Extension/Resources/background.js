3d8fd19df25dff66a7d666991d675c4cde3da808
Matthew Todd <todd@cockroachlabs.com>
2021-06-25 12:58:43 -0400

    Try hacking on this Safari extension.

--- libexec/Go/Go Extension/Resources/background.js
+++ libexec/Go/Go Extension/Resources/background.js
@@ -1,6 +1,35 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+// https://github.com/trotto/browser-extension/blob/master/src/background/background.js
+class Background {
+  constructor() {
+    this.api = browser;
+  }
 
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
-});
+  run() {
+    console.log("Running!");
+    this.registerListeners();
+  }
+
+  registerListeners() {
+    this.api.webRequest.onBeforeRequest.addListener(
+      this.rewriteGoRequests.bind(this),
+      { urls: ["<all_urls>"] },
+      []
+    );
+  }
+
+  // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/onBeforeRequest#details
+  rewriteGoRequests(details) {
+    console.log("Yay!");
+    console.log(details);
+    const redirectUrl = "https://go.crdb.dev/wiki";
+    // It seems that, even with a Promise here, Safari no likey.
+    // I may need to switch to a native extension, as outlined here:
+    // https://stackoverflow.com/questions/40959635/safari-extension-and-chrome-webrequest-onbeforerequest-addlistener
+    return new Promise((resolve, reject) => {
+      console.log("Resolving?");
+      resolve({ redirectUrl });
+    });
+  }
+}
+
+new Background().run();
