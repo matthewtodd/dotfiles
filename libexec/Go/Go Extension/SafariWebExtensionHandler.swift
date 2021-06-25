44a420d30bfe1ef9081f8f967c23b25b561f3ad8
Matthew Todd <todd@cockroachlabs.com>
2021-06-25 10:48:33 -0400

    Starting a Safari extension for go links.

---
+++ libexec/Go/Go Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,26 @@
+//
+//  SafariWebExtensionHandler.swift
+//  Go Extension
+//
+//  Created by Matthew Todd on 6/25/21.
+//
+
+import SafariServices
+import os.log
+
+let SFExtensionMessageKey = "message"
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+	func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+    
+}
