36a49f61e1cdec15843242c0f741affb45c5f42e
Matthew Todd <todd@cockroachlabs.com>
2021-08-10 11:08:33 -0400

    Put managed-service/bin in my PATH.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -10,6 +10,6 @@ set -x XDG_CACHE_HOME $HOME/Library/Caches
 set -x XDG_DATA_HOME $HOME/Library/Application\ Support
 set -x VIMINIT ":source $XDG_CONFIG_HOME/vim/vimrc"
 
-set -g fish_user_paths "$HOME/.local/bin" /usr/local/opt/ruby/bin /usr/local/opt/make/libexec/gnubin /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+set -g fish_user_paths "$HOME/.local/bin" /usr/local/opt/ruby/bin /usr/local/opt/make/libexec/gnubin /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin $GOPATH/src/github.com/cockroachlabs/managed-service/bin
 
 alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/config"
