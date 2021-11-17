4c405f3e1dc00f64b448a9600e52ae5bed876c5a
Matthew Todd <todd@cockroachlabs.com>
2021-11-17 15:01:19 -0500

    Upgrade to go 1.17.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -2,7 +2,7 @@ set -x CLICOLOR yes
 set -x EDITOR nvim
 set -x FZF_DEFAULT_COMMAND 'fd --hidden --type f'
 set -x GOPATH $HOME/go
-set -x GOROOT /usr/local/opt/go@1.16/libexec
+set -x GOROOT /usr/local/opt/go@1.17/libexec
 set -x LESSHISTFILE -
 set -x MACOSX_DEPLOYMENT_TARGET (sw_vers -productVersion)
 set -x XDG_CONFIG_HOME $HOME/.config
@@ -11,7 +11,7 @@ set -x XDG_DATA_HOME $HOME/Library/Application\ Support
 
 fish_add_path -ag $HOME/.local/bin
 fish_add_path -ag /usr/local/opt/ruby/bin
-fish_add_path -ag /usr/local/opt/go@1.16/bin
+fish_add_path -ag /usr/local/opt/go@1.17/bin
 fish_add_path -ag /usr/local/opt/make/libexec/gnubin
 fish_add_path -ag /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
 fish_add_path -ag $GOPATH/bin
