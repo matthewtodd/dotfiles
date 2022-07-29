13d855bb12f53483950e999071250203230d14f5
Matthew Todd <matthew@matthewtodd.org>
2022-07-29 12:06:05 -0400

    Go 1.18.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -2,7 +2,7 @@ set -x CLICOLOR yes
 set -x EDITOR nvim
 set -x FZF_DEFAULT_COMMAND 'fd --hidden --type f'
 set -x GOPATH $HOME/go
-set -x GOROOT /usr/local/opt/go@1.17/libexec
+set -x GOROOT /usr/local/opt/go@1.18/libexec
 set -x LESSHISTFILE -
 set -x MACOSX_DEPLOYMENT_TARGET (sw_vers -productVersion)
 set -x XDG_CONFIG_HOME $HOME/.config
@@ -10,7 +10,7 @@ set -x XDG_CACHE_HOME $HOME/Library/Caches
 set -x XDG_DATA_HOME $HOME/Library/Application\ Support
 
 fish_add_path -ag $HOME/.local/bin
-fish_add_path -ag /usr/local/opt/go@1.17/bin
+fish_add_path -ag /usr/local/opt/go@1.18/bin
 fish_add_path -ag /usr/local/opt/make/libexec/gnubin
 fish_add_path -ag /usr/local/opt/node@16/bin
 fish_add_path -ag /usr/local/opt/python@3.9/libexec/bin
