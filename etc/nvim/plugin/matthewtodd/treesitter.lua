8586ec2468e016d72131f94a3a68f4e7877043d8
Matthew Todd <matthew@matthewtodd.org>
2022-11-10 10:30:15 -0500

    Remove treesitter for now -- lots of breakage.
    I'll dig back in another time.

--- etc/nvim/plugin/matthewtodd/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require('nvim-treesitter.configs').setup({
-  -- https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
-  ensure_installed = "all",
-
-  -- https://github.com/nvim-treesitter/nvim-treesitter#available-modules
-  highlight = {
-    enable = true,
-  },
-})
