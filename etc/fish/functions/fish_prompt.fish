03485d735c7f23c4b59fb3852b10eecd5db62c31
Matthew Todd <matthew@matthewtodd.org>
2023-08-18 13:05:33 -0400

    Show latest background job in shell prompt.
    I get confused about whether I've backgrounded vim all the time.

--- etc/fish/functions/fish_prompt.fish
+++ etc/fish/functions/fish_prompt.fish
@@ -12,6 +12,16 @@ set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_diverged '+'
 
 function fish_prompt
-  printf '%s%s ' (prompt_pwd | sed -e 's/.*\//…\//') (fish_git_prompt)
+  # Show any backgrounded job, since I get confused about whether I've backgrounded vim all the time.
+  # jobs -c shows the command name only, not the PID or anything.
+  # jobs -l shows only the latest background job, which is probably good enough for me.
+  # Use tail -1 because jobs still prints out a Command header.
+  set -l last_job_command (jobs -cl | tail -1)
+
+  if test -n $last_job_command
+    set last_job " "$(set_color brmagenta)"["$last_job_command"]"$(set_color normal)
+  end
+
+  printf '%s%s%s ' (prompt_pwd | sed -e 's/.*\//…\//') (fish_git_prompt) $last_job
 end
 
