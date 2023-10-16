746f08862f8f379ea86b6182b7cd96c27ed247be
Matthew Todd <matthew@matthewtodd.org>
2023-10-16 09:22:10 -0400

    Back to original dark wallpaper.
    With my desk moved, the overhead light no longer interferes with the
    color at the top.

--- src/dynamic_desktop.swift
+++ src/dynamic_desktop.swift
@@ -100,10 +100,10 @@ let desktop = DynamicDesktop(
 
     dark: await render(raycast(
         stops: [
-            Solarized.base03.at(0.10),
-            Solarized.orange.at(0.15),
-            Solarized.base01.at(0.20),
-            Solarized.base03.at(0.25)
+            Solarized.base03.at(0),
+            Solarized.base01.at(0.75),
+            Solarized.green.at(0.8),
+            Solarized.base03.at(0.85)
         ],
         center: UnitPoint(x: 0.5, y: 0.40),
         angle: .degrees(0)
