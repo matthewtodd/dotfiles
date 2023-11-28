2e86a8f526e4ae34b842c03813751d5c0b967e4a
Matthew Todd <matthew@matthewtodd.org>
2023-11-28 14:51:00 -0500

    Add some Waiting for choices!

--- etc/hammerspoon/Spoons/WaitingFor.spoon/init.lua
+++ etc/hammerspoon/Spoons/WaitingFor.spoon/init.lua
@@ -7,14 +7,27 @@ function insertText(prefix)
   when = string.gsub(when, "^0", "")
   when = string.gsub(when, "AM", "am")
   when = string.gsub(when, "PM", "pm")
-  local message = string.format("(%s %s.)", prefix or "Wrote", when)
+  local message = string.format("(%s %s.)", prefix, when)
   hs.eventtap.keyStrokes(message)
+end
+
+function waitingFor(modal, prefix)
+  return function()
+    insertText(prefix)
+    modal:exit()
+  end
 end
 
 function obj:bindHotkeys(mapping)
   local mods = mapping.insertText[1]
   local key = mapping.insertText[2]
-  hs.hotkey.bind(mods, key, insertText)
+  obj.modal = hs.hotkey.modal.new(mods, key)
+  obj.modal:bind("", "escape", function() obj.modal:exit() end)
+  obj.modal:bind("", "o", waitingFor(obj.modal, "Ordered"))
+  obj.modal:bind("", "s", waitingFor(obj.modal, "Spoke"))
+  obj.modal:bind("", "t", waitingFor(obj.modal, "Texted"))
+  obj.modal:bind("", "v", waitingFor(obj.modal, "Left voicemail"))
+  obj.modal:bind("", "w", waitingFor(obj.modal, "Wrote"))
 end
 
 return obj
