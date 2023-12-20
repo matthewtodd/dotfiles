f8130e86a258c5436c840cfd64a956663a09f326
Matthew Todd <matthew@matthewtodd.org>
2023-12-20 08:25:34 -0500

    Remove GOPACKAGESDRIVER support for now.

--- etc/nvim/plugin/matthewtodd/lspconfig.lua
+++ etc/nvim/plugin/matthewtodd/lspconfig.lua
@@ -48,32 +48,9 @@ nvim_lsp.eslint.setup({
   },
 })
 
-local file_exists = function(path)
-  local f = io.open(path, 'r')
-  if f ~= nil then
-    f.close()
-    return true
-  else
-    return false
-  end
-end
-
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#gopls
 -- https://github.com/bazelbuild/rules_go/wiki/Editor-setup
 nvim_lsp.gopls.setup({
-  on_new_config = function(config, new_root_dir)
-    -- https://github.com/cockroachdb/cockroach/commit/4d304b23442a8cab6a05a1e4bcbaee5cc5d72581
-    -- Some information about how this thing is understanding a file can be had
-    -- by running it separately from the command line, like so:
-    -- echo {} | ./build/bazelutil/gopackagesdriver.sh file=pkg/sql/crdb_internal.go
-    local gopackagesdriver = new_root_dir .. '/build/bazelutil/gopackagesdriver.sh'
-    if file_exists(gopackagesdriver) then
-      config.cmd_env = {
-        GOPACKAGESDRIVER = gopackagesdriver,
-      }
-    end
-  end,
-
   on_attach = function(client, bufnr)
     configure_defaults(client, bufnr)
     configure_format_on_save(client, bufnr)
