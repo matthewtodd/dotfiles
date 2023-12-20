d4384c5c85d96bb98726a786c81586baae12a46a
Matthew Todd <matthew@matthewtodd.org>
2023-12-20 08:44:59 -0500

    Remove these comments I don't need.

--- etc/nvim/plugin/matthewtodd/lspconfig.lua
+++ etc/nvim/plugin/matthewtodd/lspconfig.lua
@@ -1,25 +1,9 @@
 local nvim_lsp = require('lspconfig')
 
---[[
-  Built-in Commands
-  https://github.com/neovim/nvim-lspconfig/#built-in-commands
-
-  - `:LspInfo` shows the status of active and configured language servers.
-  - `:LspStart <config_name>` Start the requested server name. Will only
-     successfully start if the command detects a root directory matching the
-     current config. Pass autostart = false to your .setup{} call for a language
-     server if you would like to launch clients solely with this command.
-     Defaults to all servers matching current buffer filetype.
-  - `:LspStop <client_id>` Defaults to stopping all buffer clients.
-  - `:LspRestart <client_id>` Defaults to restarting all buffer clients.
---]]
-
-
 local configure_format_on_save = function(client, bufnr)
   vim.api.nvim_command(string.format('autocmd BufWritePre <buffer=%d> %s', bufnr, 'lua vim.lsp.buf.format({ async = false })'))
 end
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#eslint
 nvim_lsp.eslint.setup({
   on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
@@ -33,8 +17,6 @@ nvim_lsp.eslint.setup({
   },
 })
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#gopls
--- https://github.com/bazelbuild/rules_go/wiki/Editor-setup
 nvim_lsp.gopls.setup({
   on_attach = configure_format_on_save,
 
@@ -64,17 +46,14 @@ nvim_lsp.gopls.setup({
   },
 })
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#html
 nvim_lsp.html.setup({
   on_attach = configure_format_on_save,
 })
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#standardrb
 nvim_lsp.standardrb.setup({
   on_attach = configure_format_on_save,
 })
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
 nvim_lsp.tsserver.setup({
   settings = {
     -- https://github.com/typescript-language-server/typescript-language-server#initializationoptions
@@ -92,8 +71,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- https://github.com/neovim/nvim-lspconfig/#keybindings-and-completion
-    -- :help lsp-buf for more
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
