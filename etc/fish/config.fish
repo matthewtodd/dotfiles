c6e1f0481b3bd458723965aef77a164a28f3c99e
Matthew Todd <mt@bridge.xyz>
2024-04-01 16:43:38 -0400

    Set OBJC_DISABLE_INITIALIZE_FORK_SAFETY.
    Squelches some warnings like these:
    
        objc[18501]: +[__NSCFConstantString initialize] may have been in progress in another thread when fork() was called.
        objc[18501]: +[__NSCFConstantString initialize] may have been in progress in another thread when fork() was called. We cannot safely call it or ignore it in the fork() child process. Crashing instead. Set a breakpoint on objc_initializeAfterForkError to debug.
    
    https://github.com/rails/rails/issues/38560

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -8,6 +8,8 @@ set -x HOMEBREW_REPOSITORY '/opt/homebrew';
 set -x INFOPATH '/opt/homebrew/share/info:'
 set -x LESSHISTFILE -
 set -x MANPATH '/opt/homebrew/share/man:'
+# https://github.com/rails/rails/issues/38560
+set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_DATA_HOME $HOME/.local/share
