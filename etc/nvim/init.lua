df06f9d025d87693722601bb845d79066386f213
Matthew Todd <mt@bridge.xyz>
2024-05-06 17:05:35 -0700

    Swap in telescope for stock lsp actions.

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -42,14 +42,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gd', builtin.lsp_definitions, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gi', builtin.lsp_implementations, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>D', builtin.lsp_type_definitions, opts)
     vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
     vim.keymap.set({'n', 'v'}, '<leader>c', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gr', builtin.lsp_references, opts)
 
     vim.api.nvim_create_autocmd('BufWritePre', {
       buffer = bufnr,
