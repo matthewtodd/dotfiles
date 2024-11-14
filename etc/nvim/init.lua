98d3b3879e0f9a00985b3b448d4a0a0339ab702e
Matthew Todd <mt@bridge.xyz>
2024-11-14 11:06:36 -0800

    Easier window movements

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -21,6 +21,10 @@ vim.keymap.set('n', '<leader>q', telescope.diagnostics)
 vim.keymap.set('n', '<leader>t', telescope.git_files)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<c-h>', '<c-w>h')
+vim.keymap.set('n', '<c-j>', '<c-w>j')
+vim.keymap.set('n', '<c-k>', '<c-w>k')
+vim.keymap.set('n', '<c-l>', '<c-w>l')
 vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>')
 
 -- colorscheme
