33146f6836219c3016e9296af07ec911acfa0a40
Matthew Todd <mt@bridge.xyz>
2025-01-27 21:31:01 -0500

    Drop OBJC_DISABLE_INITIALIZE_FORK_SAFETY
    And be explicit about global scope for these variables

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,19 +1,17 @@
-set -x CDPATH ~/Code
-set -x CLICOLOR yes
-set -x EDITOR nvim
-set -x FZF_DEFAULT_COMMAND 'fd --hidden --type f'
-set -x HOMEBREW_CELLAR '/opt/homebrew/Cellar';
-set -x HOMEBREW_PREFIX '/opt/homebrew';
-set -x HOMEBREW_REPOSITORY '/opt/homebrew';
-set -x INFOPATH '/opt/homebrew/share/info:'
-set -x LESSHISTFILE -
-set -x MANPATH '/opt/homebrew/share/man:'
-# https://github.com/rails/rails/issues/38560
-set -x OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
-set -x XDG_CACHE_HOME $HOME/.cache
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_DATA_HOME $HOME/.local/share
-set -x XDG_STATE_HOME $HOME/.local/state
+set -gx CDPATH ~/Code
+set -gx CLICOLOR yes
+set -gx EDITOR nvim
+set -gx FZF_DEFAULT_COMMAND 'fd --hidden --type f'
+set -gx HOMEBREW_CELLAR '/opt/homebrew/Cellar';
+set -gx HOMEBREW_PREFIX '/opt/homebrew';
+set -gx HOMEBREW_REPOSITORY '/opt/homebrew';
+set -gx INFOPATH '/opt/homebrew/share/info:'
+set -gx LESSHISTFILE -
+set -gx MANPATH '/opt/homebrew/share/man:'
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_STATE_HOME $HOME/.local/state
 
 fish_add_path -ag $HOME/.local/bin
 fish_add_path -ag /opt/homebrew/bin
