5000932562037a4efc8d5f6ad58f861083ee3f3c
Matthew Todd <matthew@matthewtodd.org>
2025-05-17 06:52:20 -0400

    Try blink docs and signature help

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -68,6 +68,14 @@ vim.api.nvim_create_autocmd({ 'VimResized' }, {
 })
 
 -- plugin settings
+require('blink.cmp').setup {
+  completion = {
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+  },
+
+  signature = { enabled = true },
+}
+
 require('telescope').setup {
   extensions = {
     ['ui-select'] = {
