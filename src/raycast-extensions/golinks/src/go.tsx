d7dfaa733121fa11f1b0f2e193f178a52dac05ce
Matthew Todd <mt@bridge.xyz>
2025-05-19 09:38:59 -0400

    Try to support paths in golinks

--- src/raycast-extensions/golinks/src/go.tsx
+++ src/raycast-extensions/golinks/src/go.tsx
@@ -1,5 +1,6 @@
 import { Action, ActionPanel, Icon, List } from "@raycast/api";
 import { getFavicon, useFetch } from "@raycast/utils";
+import { useState } from "react";
 
 type GoLink = {
   Short: string;
@@ -8,6 +9,64 @@ type GoLink = {
   LastEdit: string;
   Owner: string;
 };
+
+const favicon = (string: string) => {
+  const match = string.match(/https?:\/\/.*?\//);
+
+  if (match) {
+    return getFavicon(match[0]);
+  }
+}
+
+
+function Path({ golink }: { golink: GoLink }) {
+  const [searchText, setSearchText] = useState("");
+
+  return (
+    <List filtering={false} searchBarPlaceholder={golink.Short} onSearchTextChange={setSearchText}>
+      <List.Item
+        title={golink.Short + "/" + searchText}
+        subtitle={golink.Long}
+        icon={favicon(golink.Long)}
+        accessories={[{ icon: Icon.Person, text: golink.Owner }]}
+        actions={
+          <ActionPanel>
+            <Action.OpenInBrowser url={"http://go/" + golink.Short + "/" + searchText} />
+            <Action.OpenInBrowser url={"http://go/.detail/" + golink.Short} title="Open Details in Browser" />
+            <Action.CopyToClipboard content={"http://go/" + golink.Short + "/" + searchText} shortcut={{ modifiers: ["cmd"], key: "c" }} />
+          </ActionPanel>
+        }
+      />
+    </List>
+  );
+}
+
+function Item({ golink }: { golink: GoLink }) {
+  const template = golink.Long.match(/{{/);
+
+  return (
+    <List.Item
+      key={golink.Short}
+      title={golink.Short}
+      subtitle={golink.Long}
+      icon={favicon(golink.Long)}
+      accessories={[{ icon: Icon.Person, text: golink.Owner }]}
+      actions={
+        template ? (
+          <ActionPanel>
+            <Action.Push title="Append Path" target={<Path golink={golink} />} />
+          </ActionPanel>
+        ) : (
+          <ActionPanel>
+            <Action.OpenInBrowser url={"http://go/" + golink.Short} />
+            <Action.OpenInBrowser url={"http://go/.detail/" + golink.Short} title="Open Details in Browser" />
+            <Action.CopyToClipboard content={"http://go/" + golink.Short} shortcut={{ modifiers: ["cmd"], key: "c" }} />
+          </ActionPanel>
+        )
+      }
+    />
+  )
+}
 
 export default function Command() {
   const { data, isLoading } = useFetch("http://go/.export", {
@@ -24,22 +83,12 @@ export default function Command() {
     keepPreviousData: true,
   });
 
+
+  // https://developers.raycast.com/
   return (
     <List isLoading={isLoading}>
-      {(data as GoLink[]).map((d) => (
-        <List.Item
-          key={d.Short}
-          title={d.Short}
-          subtitle={d.Long}
-          icon={getFavicon(d.Long)}
-          accessories={[{ icon: Icon.Person, text: d.Owner }]}
-          actions={
-            <ActionPanel>
-              <Action.OpenInBrowser url={"http://go/" + d.Short} />
-              <Action.OpenInBrowser url={"http://go/.detail/" + d.Short} title="Open Details in Browser" />
-            </ActionPanel>
-          }
-        />
+      {(data as GoLink[]).map((golink) => (
+        <Item golink={golink} />
       ))}
     </List>
   );
