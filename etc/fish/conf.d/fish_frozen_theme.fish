4c09d8c0402f856b6a800705d87fcaa1664a50e1
Matthew Todd <matthew@matthewtodd.org>
2025-12-30 13:33:52 -0500

    fish: upgraded to version 4.3:
    * Color variables are no longer set in universal scope by default.
      Migrated them to global variables set in ~/.config/fish/conf.d/fish_frozen_theme.fish
      To restore syntax highlighting in other fish sessions, please restart them.
    * The fish_key_bindings variable is no longer set in universal scope by default.
      Migrated it to a global variable set in  ~/.config/fish/conf.d/fish_frozen_key_bindings.fish

---
+++ etc/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,38 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion brblue
+set --global fish_color_cancel -r
+set --global fish_color_command blue
+set --global fish_color_comment brcyan
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end green
+set --global fish_color_error red
+set --global fish_color_escape cyan
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_match magenta
+set --global fish_color_normal normal
+set --global fish_color_operator green
+set --global fish_color_param cyan
+set --global fish_color_quote yellow
+set --global fish_color_redirection cyan
+set --global fish_color_search_match white --background=brgreen
+set --global fish_color_selection white '' --background=brgreen
+set --global fish_color_status red
+set --global fish_color_user green
+set --global fish_color_valid_path --underline
+set --global fish_pager_color_completion
+set --global fish_pager_color_description yellow
+set --global fish_pager_color_prefix magenta --bold
+set --global fish_pager_color_progress white --background=cyan
+set --global fish_pager_color_selected_background -r
