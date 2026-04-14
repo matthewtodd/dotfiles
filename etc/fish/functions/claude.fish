5174c2aa5f804502cfb3b790af2a68ace35e98d4
Matthew Todd <mt@bridge.xyz>
2026-04-14 09:56:36 -0400

    Don't bother with op since allium's disabled

--- etc/fish/functions/claude.fish
+++ etc/fish/functions/claude.fish
@@ -1,7 +1,11 @@
 # The usual way to run claude, see go/ai
 function claude
-    set -fx ALLIUM_API_KEY 'op://Employee/Allium API Key/credential'
+    # Use this if I ever want the allium mcp:
+    # set -fx ALLIUM_API_KEY 'op://Employee/Allium API Key/credential'
+    # op run --no-masking -- \
+        # claude --dangerously-skip-permissions $argv
 
-    op run --no-masking -- \
-        claude --dangerously-skip-permissions $argv
+    # Use a dummy ALLIUM_API_KEY to avoid warnings:
+    set -fx ALLIUM_API_KEY ''
+    command claude --dangerously-skip-permissions $argv
 end
