f5806bfc089c7b2fc9ed3bde9e829d19f8d37a4a
Matthew Todd <matthew@matthewtodd.org>
2026-04-30 08:44:48 -0400

    Revert "I might like claude next to a simple vim"
    This reverts commit cb7d63d8ed05c184600a2063db84e138335e5bc1.

--- etc/hammerspoon/init.lua
+++ etc/hammerspoon/init.lua
@@ -35,7 +35,6 @@ local applicationConfig = {
 
   __default__ = {
     Discord     = { SIDEBAR },
-    Ghostty     = { LEFT, CENTER, center(2 / 3), RIGHT },
     Ivory       = { LEFT, CENTER_SMALL },
     Mail        = { LEFT, SIDEBAR, CENTER_SMALL, RIGHT },
     Messages    = { LEFT, CENTER_SMALL },
